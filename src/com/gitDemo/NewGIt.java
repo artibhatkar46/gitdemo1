0a8d4e047d35ee4d00c993c6b1750585eee30afb
Ankush141090 <ankushdolas2409@gmail.com>
2023-11-03 15:06:41 +0530

    Commiting code to new branch

---
+++ src/com/gitDemo/NewGIt.java
@@ -0,0 +1,19 @@
+package com.gitDemo;
+
+public class NewGIt {
+
+	public static void main(String[] args) {
+
+		int num = 7;
+
+		if (num % 2 == 0) 
+		{
+			System.out.println("even");
+		}
+
+		else 
+		{
+			System.out.println("odd");
+		}
+	}
+}
